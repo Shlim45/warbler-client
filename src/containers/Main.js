c82ce5176f4b425af6fd9191f9204fcfdef97465
Shlim45 <jhawranko@gmail.com>
2018-03-26 20:09:34 -0400

    Add AuthForm and front-end auth logic

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -2,12 +2,45 @@ import React from 'react';
 import { Switch, Route, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import Homepage from '../components/Homepage';
+import AuthForm from '../components/AuthForm';
+import { authUser } from '../store/actions/auth';
+import { removeError } from '../store/actions/errors';
 
 const Main = props => {
+    const { authUser, errors, removeError } = props;
     return (
         <div className="container">
             <Switch>
                 <Route exact path="/" render={props => <Homepage {...props} />} />
+                <Route
+                    exact
+                    path="/signin"
+                    render={props => (
+                        <AuthForm
+                            removeError={removeError}
+                            errors={errors}
+                            onAuth={authUser}
+                            buttonText="Log in"
+                            heading="Welcome Back."
+                            {...props}
+                        />
+                    )}
+                />
+                <Route
+                    exact
+                    path="/signup"
+                    render={props => (
+                        <AuthForm
+                            removeError={removeError}
+                            errors={errors}
+                            onAuth={authUser}
+                            signUp
+                            buttonText="Register"
+                            heading="Join Warbler today!"
+                            {...props}
+                        />
+                    )}
+                />
             </Switch>
         </div>
     );
@@ -16,7 +49,8 @@ const Main = props => {
 function mapStateToProps(state) {
     return {
         currentUser: state.currentUser,
+        errors: state.errors,
     };
 }
 
-export default withRouter(connect(mapStateToProps, null)(Main));
+export default withRouter(connect(mapStateToProps, { authUser, removeError })(Main));
